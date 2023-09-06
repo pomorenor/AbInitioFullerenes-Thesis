415e6d6dce18489858c64a7455e703fcff2fa3cf
Pohl <linux-pohl-v2@ramita.168.0.9>
2023-09-06 15:58:42 -0500

    Edited file for changing coordinates

--- Calculations/change_coordinates.py
+++ Calculations/change_coordinates.py
@@ -1,15 +1,24 @@
 import sys
 import re
 
-def change_coordinates(file_path, atom_line, x, y, z):
+def change_coordinates(file_path, atom_line, dx, dy, dz):
 
     with open(file_path, mode='r') as file:
         lines = file.readlines()
         atom_coordinates = lines[atom_line - 1]
         atom_coordinates2 = re.split(r'(\s+)', atom_coordinates)
-        atom_coordinates2[4] = x
-        atom_coordinates2[6] = y
-        atom_coordinates2[8] = z
+
+        initial_x = float(atom_coordinates2[4])
+        initial_y = float(atom_coordinates2[6])
+        initial_z = float(atom_coordinates2[8])
+
+        new_x = initial_x + dx
+        new_y = initial_y + dx
+        new_z = initial_z + dx
+
+        atom_coordinates2[4] = str(new_x)
+        atom_coordinates2[6] = str(new_y)
+        atom_coordinates2[8] = str(new_z)
 #        print(atom_coordinates2)
         templine = ''.join(map(str,atom_coordinates2))
 
