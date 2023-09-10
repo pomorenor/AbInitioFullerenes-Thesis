e94d10dba74fbbd929f1a2eabcd9672447e297aa
Pohl <linux-pohl-v2@ramita.168.0.9>
2023-09-10 18:28:11 -0500

    Repaired these files

--- Calculations/change_coordinates.py
+++ Calculations/change_coordinates.py
@@ -1,7 +1,7 @@
 import sys
 import re
 
-def change_coordinates(file_path, atom_line, dx, dy, dz):
+def change_coordinates(file_path, atom_line, electron_line, dx, dy, dz):
 
     with open(file_path, mode='r') as file:
         lines = file.readlines()
@@ -12,17 +12,37 @@ def change_coordinates(file_path, atom_line, dx, dy, dz):
         initial_y = float(atom_coordinates2[6])
         initial_z = float(atom_coordinates2[8])
 
-        new_x = initial_x + dx
-        new_y = initial_y + dx
-        new_z = initial_z + dx
+        initial_x += dx
+        initial_y += dy
+        initial_z += dz
 
-        atom_coordinates2[4] = str(new_x)
-        atom_coordinates2[6] = str(new_y)
-        atom_coordinates2[8] = str(new_z)
+        atom_coordinates2[4] = str(initial_x)
+        atom_coordinates2[6] = str(initial_y)
+        atom_coordinates2[8] = str(initial_z)
 #        print(atom_coordinates2)
         templine = ''.join(map(str,atom_coordinates2))
 
+        ## Now we do the same for the electrons
+
+        electron_coordinates = lines[electron_line - 1]
+        electron_coordinates_2 = re.split(r'(\s+)', electron_coordinates)
+
+        initial_x_el = float(electron_coordinates_2[4])
+        initial_y_el = float(electron_coordinates_2[6])
+        initial_z_el = float(electron_coordinates_2[8])
+
+        initial_x_el += dx
+        initial_y_el += dy
+        initial_z_el += dz
+
+        electron_coordinates_2[4] = str(initial_x_el)
+        electron_coordinates_2[6] = str(initial_y_el)
+        electron_coordinates_2[8] = str(initial_z_el)
+
+        templine2 = ''.join(map(str,electron_coordinates_2))
+
     lines[atom_line - 1] = templine
+    lines[electron_line - 1] = templine2
 
     with open(file_path, mode='w') as file:
         file.writelines(lines)
@@ -32,13 +52,13 @@ def change_coordinates(file_path, atom_line, dx, dy, dz):
 
 
 path = sys.argv[1]
-increment = float(sys.argv[2])
+atom_line = int(sys.argv[2])
+electron_line = int(sys.argv[3])
+dx = float(sys.argv[4])
+dy = float(sys.argv[5])
+dz = float(sys.argv[6])
 
 
-dx = increment
-dy = dx
-dz = dx
 
-
-path = "/home/linux-pohl-v2/Escritorio/AbInitioFullerenes-Thesis/Calculations/water2.lowdin"
-change_coordinates(path,4, dx, dy, dz)
+#path = "/home/linux-pohl-v2/Escritorio/AbInitioFullerenes-Thesis/Calculations/water2.lowdin"
+change_coordinates(path,atom_line,electron_line, dx, dy, dz)
--- Calculations/pes_scan.sh
+++ Calculations/pes_scan.sh
@@ -6,16 +6,20 @@
 
 PYTHONVER="python3"
 
-DATA="/home/linux-pohl-v2/Escritorio/AbInitioFullerenes-Thesis/Calculations/water2.lowdin"
+DATA="/home/linux-pohl-v2/Escritorio/AbInitioFullerenes-Thesis/Calculations/pes_test.lowdin"
 CHCOOR="change_coordinates.py"
 REENER="extract_lowdin_energy.py"
 
 DX="0.1"
+DY="0.0"
+DZ="0.0"
 NUMSTEPS="20"
 
+ALINE="6"
+ELINE="4"
 
 for ((ii = 0; ii < $NUMSTEPS; ++ii)); do
-	$PYTHONVER $CHCOOR $DATA $DX
+	$PYTHONVER $CHCOOR $DATA $ALINE $ELINE $DX $DY $DZ
        		
 done
 
