71f680f0838b70c638c699b4db30d8a35124c844
Pohl <linux-pohl-v2@ramita.168.0.9>
2023-09-10 19:00:51 -0500

    The pes scan script is almost ready, only lasts to correct the print error

--- Calculations/pes_scan.sh
+++ Calculations/pes_scan.sh
@@ -5,22 +5,26 @@
 #REENER means retrieve energy
 
 PYTHONVER="python3"
+LOWDINVER="lowdin2"
 
-DATA="/home/linux-pohl-v2/Escritorio/AbInitioFullerenes-Thesis/Calculations/pes_test.lowdin"
+INPUT="/home/linux-pohl-v2/Escritorio/AbInitioFullerenes-Thesis/Calculations/pes_test.lowdin"
+OUTPUT="/home/linux-pohl-v2/Escritorio/AbInitioFullerenes-Thesis/Calculations/pes_test.out"
 CHCOOR="change_coordinates.py"
 REENER="extract_lowdin_energy.py"
 
-DX="0.1"
+DX="0.2"
 DY="0.0"
 DZ="0.0"
-NUMSTEPS="20"
+NUMSTEPS="10"
 
 ALINE="6"
 ELINE="4"
 
 for ((ii = 0; ii < $NUMSTEPS; ++ii)); do
-	$PYTHONVER $CHCOOR $DATA $ALINE $ELINE $DX $DY $DZ
-       		
+	$PYTHONVER $CHCOOR $INPUT $ALINE $ELINE $DX $DY $DZ
+	$LOWDINVER -i $INPUT
+	ENERGY=$($PYTHONVER $REENER $OUTPUT)
+	echo $ENERGY
 done
 
 
