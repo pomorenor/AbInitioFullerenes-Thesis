6658472813df7f02ef38250a9c24f787d44b8867
Pohl <Pohl>
2024-01-15 10:10:33 -0500

    Continued improving the script

--- Code/NOCILevel/change_lebedev_grid.py
+++ Code/NOCILevel/change_lebedev_grid.py
@@ -1,10 +1,14 @@
 import sys 
 
-def change_grid(file_path):
+def change_grid(file_path, linenum, GridPoints):
 	num_lines = 0
-	with open(file_path, 'w') as file:
+	with open(file_path, 'r') as file:
 		lines = file.readlines()
-		num_lines = len(lines)
+		gridline = lines[linenum].rsplit('=')
+		newline = gridline[0] + "=" + str(GridPoints)
+		lines[linenum] = newline
+		print(newline)
 	return num_lines 
 
 path = sys.argv[1]
+change_grid(path,17,80)
