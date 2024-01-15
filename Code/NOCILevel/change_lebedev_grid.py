4f11481f5d845555181f5c0eaca171a528e7d927
Pohl <pomorenor@unal.edu.co>
2024-01-15 18:37:11 -0500

    finished the script for changing the num of points of the lebedev grid

--- Code/NOCILevel/change_lebedev_grid.py
+++ Code/NOCILevel/change_lebedev_grid.py
@@ -1,14 +1,17 @@
 import sys 
 
 def change_grid(file_path, linenum, GridPoints):
-	num_lines = 0
 	with open(file_path, 'r') as file:
 		lines = file.readlines()
-		gridline = lines[linenum].rsplit('=')
-		newline = gridline[0] + "=" + str(GridPoints)
-		lines[linenum] = newline
-		print(newline)
-	return num_lines 
+		gridline = lines[int(linenum)-1].rsplit('=')
+		newline = gridline[0] + "=" + str(GridPoints) + "\n"
+		lines[int(linenum)-1] = newline
+		
+		with open(file_path, 'w') as file:
+			file.writelines(lines)
+
 
 path = sys.argv[1]
-change_grid(path,17,80)
+linenum = sys.argv[2]
+gridpoints = sys.argv[3]
+change_grid(path,linenum, gridpoints)
