b6fc4e0906481d8ece4d48f8dc216e41bdb2e31d
Pohl <Pohl>
2024-01-16 10:58:13 -0500

    Finished the file for the lebedev grids and started working on the bash script for running it

--- Code/NOCILevel/change_lebedev_grid.py
+++ Code/NOCILevel/change_lebedev_grid.py
@@ -7,8 +7,14 @@ def change_grid(file_path, linenum, GridPoints):
 		newline = gridline[0] + "=" + str(GridPoints) + "\n"
 		lines[int(linenum)-1] = newline
 		
-		with open(file_path, 'w') as file:
-			file.writelines(lines)
+		#with open(file_path, 'w') as file:
+		#	file.writelines(lines)
+
+		newname_endung = "_" + str(GridPoints) + "_" +"gridpoints" +'.lowdin'
+		new_file_path =file_path.replace('.lowdin',newname_endung)
+
+		with open(new_file_path, 'w') as new_file:
+			new_file.writelines(lines)
 
 
 path = sys.argv[1]
---
+++ Code/NOCILevel/find_conv_grid.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+PYTHONVER="python3"
+LOWDINVER="lowdin2"
+
+CHANGEGRID="/home/pmorenor/Desktop/AbInitioFullerenes-Thesis/Code/NOCILevel/change_lebedev_grid.py"
+LINENUM="18"
+
+
+
