ecf93c13ba864143932cc20504617999ab8e1ec9
Pohl <Pohl>
2024-01-16 12:30:23 -0500

    finished find bash script

--- Code/NOCILevel/find_conv_grid.sh
+++ Code/NOCILevel/find_conv_grid.sh
@@ -3,9 +3,9 @@
 PYTHONVER="python3"
 LOWDINVER="lowdin2"
 
-ORIGINALFILE="/home/pmorenor/Desktop/AbInitioFullerenes-Thesis/Code/NOCILevel/He3-C60-1S-NOCI.lowdin"
-CHANGEGRID="/home/pmorenor/Desktop/AbInitioFullerenes-Thesis/Code/NOCILevel/change_lebedev_grid.py"
-EXTRACTENERGY="/home/pmorenor/Desktop/AnInitioFullerenes-Thesis/code/NOCILevel/extract_lowdin_energy.py"
+ORIGINALFILE="He3-C60-1S-NOCI.lowdin"
+CHANGEGRID="change_lebedev_grid.py"
+EXTRACTENERGY="extract_lowdin_energy.py"
 LINENUM="18"
 
 grids=(26 38 50 74 86 110 146 170 194 230)
@@ -14,14 +14,13 @@ FILENAME="He3-C60-1S-NOCI"
 
 #echo $FILENAME"_""2""_gridpoints"".lowdin"
 
-REGISTERFILE="home/pmorenor/Desktop/AbInitioFullerenes-Thesis/Code/NOCILevel/GRIDENERGIES.txt"
+REGISTERFILE="GRIDENERGIES.txt"
 
 for i in ${grids[@]}; do
 	$PYTHONVER $CHANGEGRID $ORIGINALFILE $LINENUM $i
-	NEWFILENAME=$FILENAME"_"$i"_gridpoints.lowdin"
-	$LOWDINVER -i $NEWFILENAME 
-	OUTPUTNAME=$FILENAME"_"$i"_gridpoints.out"	
-	$PYTHONVER $EXTRACTENERGY $OUTPUTNAME $REGISTERFILE $i
+	$LOWDINVER -i $FILENAME"_"$i"_gridpoints.lowdin" 
+	#OUTPUTNAME=$FILENAME"_"$i"_gridpoints.out"	
+	#$PYTHONVER $EXTRACTENERGY $OUTPUTNAME $REGISTERFILE $i
 done 	
 
 
