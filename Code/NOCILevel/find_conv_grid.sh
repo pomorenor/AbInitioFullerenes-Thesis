947e2206e4d5c90c895edc80b80b8c27c2ca9e92
Pohl <Pohl>
2024-01-16 11:51:12 -0500

    finished the bash script for looking convergence with number of grid points

--- Code/NOCILevel/find_conv_grid.sh
+++ Code/NOCILevel/find_conv_grid.sh
@@ -3,8 +3,26 @@
 PYTHONVER="python3"
 LOWDINVER="lowdin2"
 
+ORIGINALFILE="/home/pmorenor/Desktop/AbInitioFullerenes-Thesis/Code/NOCILevel/He3-C60-1S-NOCI.lowdin"
 CHANGEGRID="/home/pmorenor/Desktop/AbInitioFullerenes-Thesis/Code/NOCILevel/change_lebedev_grid.py"
+EXTRACTENERGY="/home/pmorenor/Desktop/AnInitioFullerenes-Thesis/code/NOCILevel/extract_lowdin_energy.py"
 LINENUM="18"
+
+grids=(26 38 50 74 86 110 146 170 194 230)
+
+FILENAME="He3-C60-1S-NOCI"
+
+#echo $FILENAME"_""2""_gridpoints"".lowdin"
+
+for i in ${grids[@]}; do
+	$PYTHONVER $CHANGEGRID $ORIGINALFILE $LINENUM $i
+	NEWFILENAME=$FILENAME"_"$i"_gridpoints.lowdin"
+	$LOWDINVER -i $NEWFILENAME 
+	OUTPUTNAME=$FILENAME"_"$i"_gridpoints.out"	
+	$PYTHONVER $EXTRACTENERGY $OUTPUTNAME
+done 	
+
+
 
 
 
