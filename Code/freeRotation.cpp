0c9fea16679a5fa58372cdba56bed0dc9b34709e
Pohl <pomorenor@unal.edu.co>
2024-02-02 15:45:13 -0500

    Kept working on the code

--- Code/freeRotation.cpp
+++ Code/freeRotation.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "csv.h"
 #include <vector>
+#include <cmath>
 
 
 
 typedef std::vector<double> positionVector;
+typedef double InertiaTensor[3][3];
+
 
 class Atom {
   public:
@@ -23,6 +26,11 @@ class Atom {
       this->mass = mass;
     }
 };
+
+void computeInertiaTensor(std::vector<Atom> &allAtoms, InertiaTensor &I);
+
+
+
 
 int main()
 {
@@ -68,8 +76,44 @@ int main()
   }
 
 
+  std::cout << allAtoms[0].r_i[1] << std::endl;
+  InertiaTensor I;
 
+  computeInertiaTensor(allAtoms, I);
 
+  for(int ii = 0; ii < 3; ++ii){
+    for(int jj = 0; jj < 3; ++jj){
+      std::cout << I[ii][jj] << std::endl;
+    }
+  }
 
   return 0;
+}
+
+
+
+void computeInertiaTensor(std::vector<Atom> &allAtoms, InertiaTensor &I)
+{
+  double Temp_01 = 0.0;
+  double Temp_02 = 0.0;
+  double Temp_12 = 0.0;
+    
+  for(int ii = 0; ii < 3; ii++){
+    I[0][0] += allAtoms[ii].mass*(std::pow(allAtoms[ii].r_i[1],2) +std::pow(allAtoms[ii].r_i[2],2));
+    I[1][1] += allAtoms[ii].mass*(std::pow(allAtoms[ii].r_i[0],2) +std::pow(allAtoms[ii].r_i[2],2));
+    I[2][2] += allAtoms[ii].mass*(std::pow(allAtoms[ii].r_i[0],2) +std::pow(allAtoms[ii].r_i[1],2));
+
+    Temp_01 += allAtoms[ii].mass*allAtoms[ii].r_i[0]*allAtoms[ii].r_i[1];
+    Temp_02 += allAtoms[ii].mass*allAtoms[ii].r_i[0]*allAtoms[ii].r_i[2];
+    Temp_12 += allAtoms[ii].mass*allAtoms[ii].r_i[1]*allAtoms[ii].r_i[2];
+  }
+
+  I[0][1] += -Temp_01;
+  I[0][2] += -Temp_02;
+  I[1][2] += -Temp_12;
+
+  I[1][0] = I[0][1];
+  I[2][0] = I[2][0];
+  I[2][1] = I[1][2];
+ 
 }
