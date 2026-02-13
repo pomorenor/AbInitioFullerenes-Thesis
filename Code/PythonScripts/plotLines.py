b91b71ddf0284e8f52e4d9e7c3dc1454c3ddd70c
Pohl Moreno <pomorenor@unal.edu.co>
2026-02-13 13:35:52 -0500

    Added function for smoother plot

--- Code/PythonScripts/plotLines.py
+++ Code/PythonScripts/plotLines.py
@@ -1,6 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+from scipy.interpolate import make_interp_spline
+
+
+def smooth_curve(x, y, points=500):
+    x_new = np.linspace(x.min(), x.max(), points)
+    spline = make_interp_spline(x, y, k=3)  # cubic spline
+    y_new = spline(x_new)
+    return x_new, y_new
+
 
 # cube filenames
 cube1 = sys.argv[1]
@@ -32,9 +41,13 @@ fig, axes = plt.subplots(3, 1, figsize=(8, 10), sharey=True)
 for data, label in zip(datasets, labels):
     x, rho_x, y, rho_y, z, rho_z = data
     
-    axes[0].plot(x, rho_x, label=label)
-    axes[1].plot(y, rho_y, label=label)
-    axes[2].plot(z, rho_z, label=label)
+    x_s, rho_x_s = smooth_curve(x, rho_x)
+    y_s, rho_y_s = smooth_curve(y, rho_y)
+    z_s, rho_z_s = smooth_curve(z, rho_z)
+
+    axes[0].plot(x_s, rho_x_s, label=label)
+    axes[1].plot(y_s, rho_y_s, label=label)
+    axes[2].plot(z_s, rho_z_s, label=label)
 
 # titles and labels
 axes[0].set_title("Density along X")
